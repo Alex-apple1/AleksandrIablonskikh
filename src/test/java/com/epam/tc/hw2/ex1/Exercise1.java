b5626d543d506358f605719464e1e074a4835001
Aleksandr Iablonskikh <Aleksandr_Iablonskikh@epam.com>
2021-08-14 09:09:42 +0300

    ex 1 5 and 11 tesks are fixed

--- src/test/java/com/epam/tc/hw2/ex1/Exercise1.java
+++ src/test/java/com/epam/tc/hw2/ex1/Exercise1.java
@@ -3,6 +3,7 @@ package com.epam.tc.hw2.ex1;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -61,55 +62,32 @@ public class Exercise1 {
         //        5. Assert that there are 4 items on the header section are displayed
         //        and they have proper texts
 
-        webElement = webDriver.findElement(By.xpath("//*[text()='Home']"));
-        String headerItemHome = webElement.getText().toUpperCase();
-        assertThat(headerItemHome)
-            .as("Header item Home")
-            .isEqualTo("HOME");
+        String[][] headerSectionArray = {
+            {"//*[text()='Home']", "HOME"},
+            {"//*[text()='Contact form']", "CONTACT FORM"},
+            {"//*[text()='Service']", "SERVICE"},
+            {"//*[text()='Metals & Colors']", "METALS & COLORS"}
+        };
 
-        webElement = webDriver.findElement(By.xpath("//*[text()='Contact form']"));
-        String headerItemContactForm = webElement.getText().toUpperCase();
-        assertThat(headerItemContactForm)
-            .as("Header item Contact form")
-            .isEqualTo("CONTACT FORM");
-
-        webElement = webDriver.findElement(By.xpath("//*[text()='Service']"));
-        String headerItemService = webElement.getText();
-        assertThat(headerItemService)
-            .as("Header item Service")
-            .isEqualToIgnoringCase("SERVICE");
-
-        webElement = webDriver.findElement(By.xpath("//*[text()='Metals & Colors']"));
-        String headerItemMetalsAndColors = webElement.getText();
-        assertThat(headerItemMetalsAndColors)
-            .as("Header item Metals & Colors")
-            .isEqualToIgnoringCase("METALS & COLORS");
+        for (String[] strings : headerSectionArray) {
+            webElement = webDriver.findElement(By.xpath(strings[0]));
+            String headerSectionText = webElement.getText().toUpperCase();
+            assertThat(headerSectionText)
+                .as("Text in the Header Section")
+                .isEqualTo(strings[1]);
+        }
 
         //        6. Assert that there are 4 images on the Index Page and they are displayed
 
-        webElement = webDriver.findElement(By.className("icons-benefit"));
-        boolean indexPageMicroscopePicture = webElement.isDisplayed();
-        assertThat(indexPageMicroscopePicture)
-            .as("Index page microscope picture")
-            .isTrue();
+        List<String> indexPageImages = List.of("icons-benefit", "icon-custom", "icon-multi", "icon-base");
 
-        webElement = webDriver.findElement(By.className("icon-custom"));
-        boolean indexPageHeadphonePicture = webElement.isDisplayed();
-        assertThat(indexPageHeadphonePicture)
-            .as("Index page headphone picture")
-            .isTrue();
-
-        webElement = webDriver.findElement(By.className("icon-multi"));
-        boolean indexPageScreenPicture = webElement.isDisplayed();
-        assertThat(indexPageScreenPicture)
-            .as("Index page screen picture")
-            .isTrue();
-
-        webElement = webDriver.findElement(By.className("icon-base"));
-        boolean indexPageRocketPicture = webElement.isDisplayed();
-        assertThat(indexPageRocketPicture)
-            .as("Index page rocket picture")
-            .isTrue();
+        for (String indexPageClassName : indexPageImages) {
+            webElement = webDriver.findElement(By.className(indexPageClassName));
+            boolean indexPagePicture = webElement.isDisplayed();
+            assertThat(indexPagePicture)
+                .as("Index page some picture")
+                .isTrue();
+        }
 
         //        7. Assert that there are 4 texts on the Index Page under icons and they have proper text
 
@@ -167,35 +145,21 @@ public class Exercise1 {
 
         //        11. Assert that there are 5 items in the Left Section are displayed and they have proper text
 
-        webElement = webDriver.findElement(By.xpath("//*[@id='mCSB_1_container']/ul/li[1]/a/span"));
-        String leftSectionHomeText = webElement.getText();
-        assertThat(leftSectionHomeText)
-            .as("Home in the Left Section")
-            .isEqualTo("Home");
+        String[][] leftSectionArr = {
+            {"//*[@id='mCSB_1_container']/ul/li[1]/a/span", "Home"},
+            {"//*[@id='mCSB_1_container']/ul/li[2]/a/span", "Contact form"},
+            {"//*[@id='mCSB_1_container']/ul/li[3]/a/span", "Service"},
+            {"//*[@id='mCSB_1_container']/ul/li[4]/a/span", "Metals & Colors"},
+            {"//*[@id='mCSB_1_container']/ul/li[5]/a/span", "Elements packs"}
+        };
 
-        webElement = webDriver.findElement(By.xpath("//*[@id='mCSB_1_container']/ul/li[2]/a/span"));
-        String leftSectionContactForm = webElement.getText();
-        assertThat(leftSectionContactForm)
-            .as("Contact form in the Left Section")
-            .isEqualTo("Contact form");
-
-        webElement = webDriver.findElement(By.xpath("//*[@id='mCSB_1_container']/ul/li[3]/a/span"));
-        String leftSectionService = webElement.getText();
-        assertThat(leftSectionService)
-            .as("Service in the Left Section")
-            .isEqualTo("Service");
-
-        webElement = webDriver.findElement(By.xpath("//*[@id='mCSB_1_container']/ul/li[4]/a/span"));
-        String leftSectionMetalsAndColors = webElement.getText();
-        assertThat(leftSectionMetalsAndColors)
-            .as("Metals & Colors in the Left Section")
-            .isEqualTo("Metals & Colors");
-
-        webElement = webDriver.findElement(By.xpath("//*[@id='mCSB_1_container']/ul/li[5]/a/span"));
-        String leftSectionElementPacks = webElement.getText();
-        assertThat(leftSectionElementPacks)
-            .as("Elements packs in the Left Section")
-            .isEqualTo("Elements packs");
+        for (String[] strings : leftSectionArr) {
+            webElement = webDriver.findElement(By.xpath(strings[0]));
+            String leftSectionText = webElement.getText();
+            assertThat(leftSectionText)
+                .as("Text in the Left Section")
+                .isEqualTo(strings[1]);
+        }
 
         //        12. Close Browser
 
@@ -204,7 +168,7 @@ public class Exercise1 {
 
     @AfterTest
     public void clear() {
-        webDriver.close();
+        webDriver.quit();
     }
 }
 
