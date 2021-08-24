9dbdd8efcf44e1b8a704982da724e0ed73c1bfa9
Aleksandr Iablonskikh <Aleksandr_Iablonskikh@epam.com>
2021-08-24 07:40:39 +0300

    for checks

--- src/main/java/com/epam/tc/hw4/utils/AttachmentUtils.java
+++ src/main/java/com/epam/tc/hw4/utils/AttachmentUtils.java
@@ -1,9 +1,6 @@
 package com.epam.tc.hw4.utils;
 
-import io.qameta.allure.Allure;
 import io.qameta.allure.Attachment;
-import java.io.InputStream;
-import java.util.List;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
