ed85187a2a2caa2199bf52564e58af537c9f6c6f
Aleksandr Iablonskikh <Aleksandr_Iablonskikh@epam.com>
2021-09-05 17:27:07 +0300

    fixed issues

--- src/test/java/com/epam/tc/hw1/CalculatorAddTest.java
+++ src/test/java/com/epam/tc/hw1/CalculatorAddTest.java
@@ -2,9 +2,6 @@ package com.epam.tc.hw1;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.epam.tat.module4.Calculator;
-import com.epam.tc.hw1.DataProviders;
-import com.epam.tc.hw1.ParentCalculatorClass;
 import org.testng.annotations.Test;
 
 public class CalculatorAddTest extends ParentCalculatorClass {
--- src/test/java/com/epam/tc/hw1/CalculatorDivideTest.java
+++ src/test/java/com/epam/tc/hw1/CalculatorDivideTest.java
@@ -2,7 +2,6 @@ package com.epam.tc.hw1;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.epam.tat.module4.Calculator;
 import org.testng.annotations.Test;
 
 public class CalculatorDivideTest extends ParentCalculatorClass {
